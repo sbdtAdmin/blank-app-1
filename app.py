ace5ee4141adfafc651a75af189a2878c7e5a6e6
Illya Lazarev <133011283+sbdtAdmin@users.noreply.github.com>
2024-09-04 21:40:28 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from login_page import login_or_register
+from wallet_page import wallet_page
+
+# Проверка состояния сессии
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+
+# Основной процесс
+if not st.session_state['logged_in']:
+    login_or_register()
+else:
+    wallet_page()

57667d14c0ab36da09f855bc9aa7a914ae45365b
Illya Lazarev <133011283+sbdtAdmin@users.noreply.github.com>
2024-09-04 21:50:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 from login_page import login_or_register
 from wallet_page import wallet_page
 
+st.markdown(
+    """
+<style>
+#MainMenu {visibility: hidden}
+footer {visibility: hidden}
+</style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Проверка состояния сессии
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
